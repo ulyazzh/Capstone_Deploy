533cb44d9e5c2067bbb756afaed5ef612ed4a0d5
ulyazzh <113325556+ulyazzh@users.noreply.github.com>
2025-06-20 12:31:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ if st.button("Prediksi Obesitas"):
         'Obesity_Type_III': "Anda mengalami Obesitas Tipe III"
     }
 
-    result_text = prediction_class.get(predicted_description, f"Kemungkinan Diabetes: {predicted_description}")
+    result_text = prediction_description.get(predicted_class, f"Kemungkinan Diabetes: {predicted_class}")
 
     st.markdown("### 🧾 Hasil Prediksi")
     st.success(f"{result_text}")
